fe271670ae781f732119d419dfeedf70b9a74ce7
Leo Work <parenyukln@gmail.com>
2019-04-23 17:03:17 +0300

    complete

--- Custom Youtube/src/js/script.js
+++ Custom Youtube/src/js/script.js
@@ -5,6 +5,7 @@ const switcher = document.getElementById('cbx'),
     modal = document.querySelector('.modal'),
     videos = document.querySelectorAll('.videos__item');
 let player;
+const videosWrapper = document.querySelector('.videos__wrapper');
 
 function bindSlideToggle(trigger, boxBody, content, openClass) {
     let button = {
@@ -65,7 +66,7 @@ switcher.addEventListener('change', () => {
 });
 bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
 
-const data = [ 
+/*const data = [ 
     ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'], 
     ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
         '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки Урок 2', 
@@ -94,7 +95,189 @@ more.addEventListener('click', () => {
         videosWrapper.appendChild(card);
         setTimeout(() => {
             card.classList.remove('videos__item-active');
-        }, 10);
-        
+        }, 10); 
+        if ( night === true ) {
+            card.style.color = '#fff';
+            card.style.color = '#fff';
+        }
+        bindNewModal(card);  
+    }
+    sliceTitle('.videos__item-descr', 70);
+});
+*/
+
+function createCard(item) {
+    let card = document.createElement('a');
+        card.classList.add('videos__item', 'videos__item-active');
+        card.setAttribute('data-url', item.contentDetails.videoId);
+        card.innerHTML = `
+            <img src="${item.snippet.thumbnails.high.url}" alt="thumb">
+            <div class="videos__item-descr">
+                ${item.snippet.title}
+            </div>
+            <div class="videos__item-views">
+                3.2 тыс просмотров
+            </div>
+        `;
+        videosWrapper.appendChild(card);
+        setTimeout(() => {
+            card.classList.remove('videos__item-active');
+        }, 10); 
+        if ( night === true ) {
+            card.style.color = '#fff';
+            card.style.color = '#fff';
+        }
+        bindNewModal(card);  
+}
+
+function createCardSearch(item) {
+    let card = document.createElement('a');
+        card.classList.add('videos__item', 'videos__item-active');
+        card.setAttribute('data-url', item.id.videoId);
+        card.innerHTML = `
+            <img src="${item.snippet.thumbnails.high.url}" alt="thumb">
+            <div class="videos__item-descr">
+                ${item.snippet.title}
+            </div>
+            <div class="videos__item-views">
+                3.2 тыс просмотров
+            </div>
+        `;
+        videosWrapper.appendChild(card);
+        setTimeout(() => {
+            card.classList.remove('videos__item-active');
+        }, 10); 
+        if ( night === true ) {
+            card.style.color = '#fff';
+            card.style.color = '#fff';
+        }
+        bindNewModal(card);  
+}
+
+function loadYoutubeData() {
+    gapi.client.init({
+        'apiKey': 'AIzaSyBcIy-FVMVCEuVfhcNlDC6gOMr6VWmW-sc',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+    }).then( () => {
+        return gapi.client.youtube.playlistItems.list({
+            'part': 'snippet,contentDetails',
+            'maxResults': '6',
+            'playlistId': 'PL3LQJkGQtzc4gsrFkm4MjWhTXhopsMgpv'
+        });
+    }).then( resp => {
+        resp.result.items.forEach( item => {
+            createCard(item);
+        });
+        sliceTitle('.videos__item-descr', 70);
+    }).catch( e => {
+        console.log(e);
+    })
+};
+
+function search(target) {
+    gapi.client.init({
+        'apiKey': 'AIzaSyBcIy-FVMVCEuVfhcNlDC6gOMr6VWmW-sc',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
+    }).then( () => {
+        return gapi.client.youtube.search.list({
+            'maxResults': '10',
+            'part': 'snippet',
+            'q': `${target}`,
+            'type': ''
+        });
+    }).then( resp => {
+        videosWrapper.innerHTML = '';
+        resp.result.items.forEach( item => {
+            createCardSearch(item);
+        });
+        sliceTitle('.videos__item-descr', 70);
+    })
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    gapi.load('client', () => {
+        search(document.querySelector('.search > input').value)
+    });
+    document.querySelector('.search > input').value = '';
+});
+
+more.addEventListener('click', () => {
+    more.remove();
+    gapi.load('client', loadYoutubeData);
+})
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        let text = item.textContent.trim();
+
+        if ( text.length < count ) {
+            return;
+        } else {
+            const str = text.slice(0,count + 1) + "...";
+            item.textContent = str;
+        }
+    });
+};
+
+sliceTitle('.videos__item-descr', 70);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModal(cards) {
+    cards.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            let dataId = item.getAttribute('data-url');
+            loadVideo(dataId);
+            openModal();
+        });
+    });
+}
+
+bindModal(videos);
+
+function bindNewModal(card) {
+    card.addEventListener('click', (event) => {
+        event.preventDefault();
+        let dataId = card.getAttribute('data-url');
+        loadVideo(dataId);
+        openModal();
+    });
+};
+
+modal.addEventListener('click', (event) => {
+    if ( !event.target.classList.contains('modal__body') ) {
+        closeModal();
     }
 });
+
+function createVideoPlayer() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    setTimeout(()=> {
+        player = new YT.Player('frame', {
+        height: '100%',
+        width: '100%',
+        videoId: 'M7lc1UVf-VE'
+      }); 
+    }, 300);
+};
+
+createVideoPlayer();
+
+function loadVideo(videoId) {
+    player.loadVideoById({
+        'videoId': `${videoId}`
+    });
+}
